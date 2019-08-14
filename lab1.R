3d3baead3e687b3577df648f41c472c244c8d40e
Michelle Bloomfield <blo16803@uvg.edu.gt>
2019-08-14 13:52:46 -0600

    se agrego tablas

--- lab1.R
+++ lab1.R
@@ -1,3 +1,14 @@
+########################################
+## Universidad del Valle de Guatemala ##
+## Laboratorio 1: Data Science        ##
+## Autores:                           ##
+##    Mayra Silva                     ##
+##    Michelle Bloomfield             ##
+########################################
+
+library(ggplot2)
+library(caret)
+
 setwd("C:/Users/User/Desktop/Data Science/Lab1-DS")
 
 ##se jalan los datos de el csv 
@@ -5,3 +16,13 @@ data <- read.csv("train_data.csv")
 
 ##Resumen de los datos
 summary(data)
+
+
+##Correlacion entre el area del lote y la distancia en la que esta conectada a la calle 
+
+correlacion <- cor(data$LotArea,data$LotFrontage, "pearson", use = "complete.obs")
+correlacion
+library("ggpubr")
+install.packages("ggpubr")
+plot(data$LotArea,data$YearBuilt)
+
