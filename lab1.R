a7dbad5a2d6a4274562c480d14372606a7ffef14
Mayra <sil17276@uvg.edu.gt>
2019-08-16 23:19:42 -0600

    update de pca...

--- lab1.R
+++ lab1.R
@@ -41,6 +41,7 @@ plot(data$YearRemodAdd, data$OverallCond)
 ## Dividiré las variables categóricas de las numericas
 dataN <- data[,c(4,5,18,19,20,21,27,35,37,38,39,44,45,46,47,48,49,50,51,52,53,55,57,60,62,63,67,68,69,70,71,72,76,77,78,81)]
 dataC <- data[,-c(4,5,18,19,20,21,27,35,37,38,39,44,45,46,47,48,49,50,51,52,53,55,57,60,62,63,67,68,69,70,71,72,76,77,78,81)]
+dataN <- na.omit(dataN)
 
 
 #Matriz de correlación
@@ -65,8 +66,10 @@ cortest.bartlett(dataN)
 #[1] 630
 
 ## PCA
-KMO(matr)
+
+KMO(dataN)
 bartlett.test(dataN)
+
 
 
 
